7ae88059aa2cae6d245ba09deaa5cd3d49d5482e
RenzoQuispe <renzo123cd@gmail.com>
2025-06-27 11:44:14 -0500

    agregar iac-mysql

---
+++ iac/variables.tf
@@ -0,0 +1,15 @@
+variable "mysql_root_password" {
+  default = "contraseña12345"
+}
+
+variable "mysql_database" {
+  default = "usuarios_service_db"
+}
+
+variable "mongo_port" {
+  default = 27017
+}
+
+variable "mysql_port" {
+  default = 3306
+}
