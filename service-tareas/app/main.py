0cb37a3a6fc75b3b9abbd43674faa581e2f7a704
RenzoQuispe <renzo123cd@gmail.com>
2025-07-15 13:14:25 -0500

    actualizar main.py , service-tareas/Dockerfile y docker-compose.yml para integrar microservicio-tareas al proyecto

--- service-tareas/app/main.py
+++ service-tareas/app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from database import db
+from controllers.tareas_controller import tareas_bp
 import os
 
 app = Flask(__name__)
+app.register_blueprint(tareas_bp)
 
 @app.route('/')
 def home():
@@ -10,4 +12,4 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 5001))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
