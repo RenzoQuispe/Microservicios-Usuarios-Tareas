66839537a56a17eba6d3b92d4e0ba8ba38b3fad0
RenzoQuispe <renzo123cd@gmail.com>
2025-07-15 16:33:00 -0500

    actualizar tareas_client.go para hacer peticiones al microservicio de tareas y obtiene las tareas del usuario

---
+++ service-reportes/services/tareas_client.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"log"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"service-reportes/models"
+)
+
+func ObtenerTareasDelUsuario(token string) ([]models.Tarea, error) {
+	log.Println("1")
+	url := os.Getenv("TAREAS_SERVICE_URL")
+	if url == "" {
+		url = "http://localhost:5001/tareas"
+	}
+	log.Println("2")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Cookie", "jwt_=" + token)
+	log.Println("3")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error en client.Do:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	log.Println("Respuesta cruda:", string(body))
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Status inesperado: %d", resp.StatusCode)
+		return nil, errors.New("respuesta no OK")
+	}
+
+	var tareas []models.Tarea
+	err = json.Unmarshal(body, &tareas)
+	if err != nil {
+		log.Println("Error al parsear JSON:", err)
+	}
+	return tareas, err
+}
