efe4bd3356e9d67656aeab8b27c4dc01186d65fd
RenzoQuispe <renzo123cd@gmail.com>
2025-07-15 03:18:10 -0500

    actualizar index.ts para usar funcionalidades de autenticacion

--- service-usuarios/src/index.ts
+++ service-usuarios/src/index.ts
@@ -1,14 +1,21 @@
 import express, { Request, Response } from 'express';
 import { testDBConnection } from './database';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Servicio de usuarios activo');
 });
+
+app.use("/api/auth", authRoutes);
 
 async function iniciarServer() {
   await testDBConnection();
